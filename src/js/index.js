4bce6d689015308a23e063434db8576030842bad
Elisa Diaz <e.vallsalfaro@gmail.com>
2023-07-18 18:33:01 +0200

    creacion pasos

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,14 @@
 import '../scss/styles.scss';
 
 // Realizar primero el CSS
+// Crear un array con palabras de cinco letras
+// Sacar una palabra aleatoria del array
+// Si la palabra que escribo contiene menos 5 letras o mas de 5 que salte un aviso de que la palabra debe contener 5 letras
+// Hacer que el texto que yo meta en el form se pinte en el grid (dando intro)
+// Hacer que cada vez que yo pinte una palabra se añada a la línea de bajo de la anterior
+// Verificar que la palabra que yo meta si las letras no están se pinten en gris (recorriendo)
+// Si la letra está en la posición correcta que se pinte verde (recorriendo)
+// Si la letra está pero no en la posición correcta que se pinte de amarillo (recorriendo)
+// Crear animaciones
+
+// cuando tu añades una clase tienes que quitar las anteriores
